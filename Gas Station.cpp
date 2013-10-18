5a06128b00142c4b642a0331522d38a9183a2a96
4T-Shirt <huachi1989@gmail.com>
2013-10-18 11:46:53 +0800

    Add 4 leetcode solutions

---
+++ Gas Station.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        // Note: The Solution object is instantiated only once and is reused by each test case. test
+        int ts = gas.size();
+        for (int i=0;i<ts;++i) cost[i] = gas[i] - cost[i];
+        int sum = cost[0],cnt = 1,precnt = 0;
+        while (precnt < ts)
+        {
+            if (sum>=0 && cnt-precnt>=ts)
+                break;
+                
+            if (cost[precnt] <0)
+            {
+                sum -= cost[precnt++];
+                sum += cost[cnt % ts];
+                cnt ++;
+                continue;
+            }
+            
+            if (sum < 0)
+            {
+                sum -= cost[precnt++];
+                sum += cost[cnt % ts];
+                cnt ++;
+            }
+            else
+            {
+                sum += cost[cnt % ts];
+                cnt ++;
+            }
+        }
+        if (cnt-precnt>=ts && sum>=0)
+            return precnt;
+        return -1;
+    }
+};
